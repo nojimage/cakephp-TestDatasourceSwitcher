e620543d143d2bf14bfb11f44002d03ec52d4c39
nojimage <nojimage@gmail.com>
2019-04-19 17:36:04 +0900

    fixes typo

--- src/Middleware/DatasourceSwitchMiddleware.php
+++ src/Middleware/DatasourceSwitchMiddleware.php
@@ -6,7 +6,7 @@
  * For full copyright and license information, please see the LICENSE.txt
  * Redistributions of files must retain the above copyright notice.
  *
- * @copyright (c) 2017, ELASTIC Consultatnts Inc. (https://elasticconsultants.com/)
+ * @copyright (c) 2017, ELASTIC Consultants Inc. (https://elasticconsultants.com/)
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
